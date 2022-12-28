7d3b6f7b43ed97e1d60d64c3e80c4807bcb82268
João Pedro <97753966+J3ipy@users.noreply.github.com>
2022-12-28 00:13:53 -0300

    Add files via upload

---
+++ assets/scripts/DbGames.js
@@ -0,0 +1,67 @@
+const highlight_games = [
+  {
+    id: '0',
+    img: './assets/img/God_of_war_img.png',
+    bg: './assets/video/God_of_war.mp4',
+    art: './assets/img/God_of_warArt.png',
+    name: 'God of War Ragnarök',
+    dev: 'Santa Monica Studio',
+    stars: '5',
+    tags: 'Ação, Rpg',
+    category: 'RPG',
+    description:
+      'God of War Ragnarök é um jogo eletrônico de ação-aventura desenvolvido pela Santa Monica Studio e publicado pela Sony Interactive Entertainment. Foi lançado em 9 de novembro de 2022 para PlayStation 4 e PlayStation 5.'
+  },
+  {
+    id: '1',
+    img: './assets/img/Nioh_2_img.png',
+    bg: './assets/video/Nioh_2.mp4',
+    art: './assets/img/Nioh_2Art.png',
+    name: 'Nioh 2',
+    dev: 'Team Ninja',
+    stars: '4',
+    tags: 'Ação, Rpg',
+    category: 'RPG',
+    description:
+      'Nioh 2 é um jogo eletrônico de RPG de ação desenvolvido pela Team Ninja e publicado pela Koei Tecmo no Japão e pela Sony Interactive Entertainment internacionalmente.'
+  },
+  {
+    id: '2',
+    img: './assets/img/Mhw_img.png',
+    bg: './assets/video/Mhw.mp4',
+    art: './assets/img/MhwArt.png',
+    name: 'Monster Hunter: World',
+    dev: 'Team Ninja',
+    stars: '4',
+    tags: 'Ação, Rpg, coop',
+    category: 'Aventura',
+    description:
+      'Monster Hunter: World é um jogo de RPG eletrônico de ação desenvolvido e publicado pela Capcom. O jogo faz parte da série Monster Hunter, foi lançado mundialmente em 26 de janeiro de 2018 para PlayStation 4 e Xbox One e em 9 de agosto de 2018.'
+  },
+  {
+    id: '3',
+    img: './assets/img/Elden_ring_img.png',
+    bg: './assets/video/Elden_ring.mp4',
+    art: './assets/img/Elden_ringArt.png',
+    name: 'Elden Ring',
+    dev: ' FromSoftware Inc',
+    stars: '5',
+    tags: 'Ação, Rpg, Pvp',
+    category: 'RPG',
+    description:
+      'LElden Ring é um jogo eletrônico de RPG de ação em terceira pessoa, desenvolvido pela FromSoftware e publicado pela Bandai Namco Entertainment. O jogo é um projeto colaborativo entre o diretor Hidetaka Miyazaki e o romancista de fantasia George R. R.'
+  },
+  {
+    id: '4',
+    img: './assets/img/Marvel_spider_man_img.png',
+    bg: './assets/video/Marvel_spider_man.mp4',
+    art: './assets/img/Marvel_spider_manArt.png',
+    name: 'Marvel’s Spider-Man',
+    dev: 'Insomniac Games',
+    stars: '3',
+    tags: 'Ação, Aventura',
+    category: 'Super-Heróis',
+    description:
+      'Spider-Man é um jogo eletrônico de ação-aventura desenvolvido pela Insomniac Games e publicado pela Sony Interactive Entertainment.'
+  }
+];
